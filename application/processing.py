45effcb17ef7d49b237cd44d1dd3698b6ef979d6
unknown <mohammednechba@gmail.com>
2023-05-22 12:35:33 +0100

    last comit

--- application/processing.py
+++ application/processing.py
@@ -8,8 +8,6 @@ API_URL_ENG = "https://api-inference.huggingface.co/models/distilbert-base-cased
 API_URL_AR = "https://api-inference.huggingface.co/models/ZeyadAhmed/AraElectra-Arabic-SQuADv2-QA"
 headers = {"Authorization": "Bearer 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"}
 
-
-
 """
 This module contains one function, `get_articles`, which performs document retrieval:
 https://en.wikipedia.org/wiki/Document_retrieval
@@ -20,13 +18,13 @@ logging.basicConfig(level=logging.INFO)
 logging.info("Running doc retrieval module")
 
 
-def get_articles(query: str, num_articles_search: int, characters_per_article: int, language: str ='english') -> List[tuple]:
+def get_articles(query: str, num_articles_search: int, characters_per_article: int, language: str = 'english') -> List[tuple]:
     """
-    This function takes a query and downloads the text if relevant Wikipedia articles.
+    This function takes a query and downloads the text of relevant Wikipedia articles.
     Parameters
     ----------
     query : str
-        A query that will be used to identify relevant wikipedia articles.
+        A query that will be used to identify relevant Wikipedia articles.
         Example: "Who did Joe Biden defeat in 2020?"
     num_articles_search : int
         The number of articles that will be searched and downloaded.
@@ -34,6 +32,8 @@ def get_articles(query: str, num_articles_search: int, characters_per_article: i
         The number of characters that will be included. The answer to a question
         is usually in the beginning of an article, so it's not necessary to search
         the entire article.
+    language : str, optional
+        The language of the Wikipedia articles. Defaults to 'english'.
     Returns
     -------
     list
@@ -41,7 +41,7 @@ def get_articles(query: str, num_articles_search: int, characters_per_article: i
         [("United States", "The United States is..."),
         ("Barack Obama", "Barack Obama is a politician..."), ...]
     """
-    if language=="arabic":
+    if language == "arabic":
         # set the language to Arabic
         wiki.set_lang("ar")
     else:
@@ -51,32 +51,24 @@ def get_articles(query: str, num_articles_search: int, characters_per_article: i
 
     # A list of article titles - these may not be the "correct" titles (see below)
     article_titles = wiki.search(query, results=num_articles_search)
-    #print("article_titles ", article_titles)
 
     # Collect tuples of (article_title, article_text)
     article_data = []
     for title in article_titles:
         try:
-            #print("wiki.page(title) ", wiki.page(title))
             # Try to get the text of the article.
             text = wiki.page(title).content[:characters_per_article]
-        except wiki.exceptions.PageError as e1:
-            # Not all the results returned by wiki.search are valid titles.
-            # wiki.suggest returns a valid title for the "incorrect" title
-            # i.e. "Joe Biden" -> "joe biden n"
-            title = wiki.suggest(title)
-            text = wiki.page(title).content[:characters_per_article]
-            
-        except wiki.DisambiguationError as e2:
-            s = random.choice(e2.options)
-            text = wiki.page(s).content[:characters_per_article]
+        except (wiki.exceptions.PageError, wiki.exceptions.DisambiguationError) as e:
+            # Skip the problematic titles and continue with the retrieval process
+            logging.warning(f"Error occurred for title '{title}': {str(e)}")
+            continue
 
         article_data.append((title, text))
 
     return dict(article_data)
 
 
-def query(payload,language='english'):
+def query(payload, language='english'):
     if language == 'english':
         response = requests.post(API_URL_ENG, headers=headers, json=payload)
     elif language == 'arabic':
@@ -85,33 +77,31 @@ def query(payload,language='english'):
 
 
 def get_answer(question, context, language='english'):
-    result = query({
+        result = query({
         "inputs": {
             "question": question,
             "context": context
-        },
-    }, language=language)
+        },}, language=language)
+        print(result["answer"])
+        return {"answer": result["answer"], "score": result["score"]}
 
-    return { "answer":result["answer"], "score":result["score"] }
 
-
-def get_best_answer(query: str, num_articles_search: int, characters_per_article: int, language: str='english'):
-    
+def get_best_answer(query: str, num_articles_search: int, characters_per_article: int, language: str = 'english'):
     print("Retrieving top documents ...")
     docs = get_articles(query, num_articles_search, characters_per_article, language=language)
-    
+
     answers = {}
-    i=1
+    i = 1
     for doc in docs:
-        print('Searching for answer in doc ',i)
+        print('Searching for answer in doc', i)
         question, context = query, docs[doc]
-        result = get_answer(question, context,language=language)
+        result = get_answer(question, context, language=language)
         if result['answer'] in answers:
             answers[result['answer']] = max(answers[result['answer']], result['score'])
         else:
             answers[result['answer']] = result['score']
-        i+=1
-        
-    answers_l = sorted(answers.items(),key=lambda x:x[1],reverse=True)
+        i += 1
+
+    answers_l = sorted(answers.items(), key=lambda x: x[1], reverse=True)
     
     return answers_l[0][0]
